2b6b354c0f57734589a5988d828544dd45e4f9af
herman72 <m.sherafati7@gmail.com>
2020-09-02 10:59:36 +0430

    Create view

--- rbrlog/models.py
+++ rbrlog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 # from django_mongoengine import Document, EmbeddedDocument, fields
 from mongoengine import fields, Document
+from datetime import datetime
 
 
 class User(Document):
@@ -8,12 +9,9 @@ class User(Document):
     def __str__(self):
         return self.name_user
 
-
-
     name_user = fields.StringField(verbose_name="Name", max_length=255)
     email_user =fields.EmailField(verbose_name="Email", required=True)
-
     ip_user = fields.StringField(verbose_name="IP", max_length=255)
-
-    created_time = fields.DateTimeField(default=datetime.datetime.now)
+    created_time = fields.DateTimeField(default=datetime.now)
+    pasword_user = fields.StringField(verbose_name="PWD", required=True)
 
--- rbrlog/views.py
+++ rbrlog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import User
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def Login(request):
+
+
+
