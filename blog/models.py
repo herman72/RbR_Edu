38d29a698c91b0f576a6c009d211635d41d8207e
herman72 <m.sherafati7@gmail.com>
2020-09-15 17:53:37 +0430

    following model

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -27,3 +27,12 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
+
+class UserFollow(User):
+
+    followers = models.ManyToManyField('UserFollow', related_name='followed_by')
+
+
+
+
