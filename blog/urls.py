029248bc605eeb1e00550a78a3174f9bd2e58565
herman72 <m.sherafati7@gmail.com>
2020-09-20 18:18:55 +0430

    change view to class base

--- blog/urls.py
+++ blog/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
 from blog import views
 
+app_name = 'blog'
+
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('signup', views.signup_view, name='signup'),
-    path('login', views.login_view, name='login'),
-    path('new', views.post_new, name='post_new'),
+    path('', views.PostList.as_view(), name='post_list'),
+    path('login', views.Login.as_view(), name='login'),
     path('logout', views.logout_view, name='logout'),
-    path('post/<int:pk>', views.post_detail, name='post_detail'),
-    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('signup', views.Register.as_view(), name='signup'),
+    path('new_post', views.NewPost.as_view(), name='new_post'),
     path('following', views.add_follower, name='following'),
+    path('post/<int:pk>', views.post_detail, name='post_detail'),
     path('request_follower', views.request_follower, name='request_follower'),
     path('request_unfollow', views.request_unfollow, name='request_unfollow'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,75 +1,159 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect, get_object_or_404
+"""import packages"""
+from django.views import View
+from django.db.models import Q
 from django.urls import reverse
 from django.utils import timezone
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from blog.forms import PostForm, CommentForm
+from django.http import HttpResponse
 from blog.models import Post, UserBlog
+from blog.forms import PostForm, CommentForm
 from django.contrib.auth import logout, login, authenticate
-from django.db.models import Q
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+"""Class Based view"""
 
 
-def post_list(request):
-    login_user = UserBlog.objects.get(username=request.user.username)
+class PostList(View):
 
-    posts = Post.objects.filter(Q(author__in=login_user.followers.all()) | Q(author=request.user))
-    return render(request, 'blog/post_list.html', context={'posts': posts, 'user': request.user})
+    def get(self, request):
+        try:
+            login_user = UserBlog.objects.get(username=request.user.username)
+            posts = Post.objects.filter(Q(author__in=login_user.followers.all()) | Q(author=request.user))
+            return render(request, template_name='blog/post_list.html', context={'posts': posts, 'user': request.user})
+        except:
+            return HttpResponse("pls sign in")
 
 
-def signup_view(request):
-    if request.method == 'POST':
+class Register(View):
+    sinup_form = UserCreationForm
 
-        form_sign = UserCreationForm(request.POST)
+    def get(self, request):
+        form_sign = self.sinup_form()
+        return render(request, template_name='blog/signup.html',
+                      context={'form_sign': form_sign})
 
+    def post(self, request):
+        form_sign = self.sinup_form(request.POST)
         if form_sign.is_valid():
             user = form_sign.save()
             logout(request)
             login(request, user)
             # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
-            return redirect(reverse('post_list'))
-            # return render(request, 'blog/post_list.html', context={'posts': posts, 'user': request.user})
-    else:
-        form_sign = UserCreationForm()
-    return render(request, template_name='blog/signup.html',
-                  context={'form_sign': form_sign})
+            return redirect(reverse('blog:post_list'))
+        else:
+            form_sign = self.sinup_form()
+            return render(request, template_name='blog/signup.html',
+                          context={'form_sign': form_sign})
 
 
-def login_view(request):
-    if request.method == 'POST':
-        form_login = AuthenticationForm(data=request.POST)
+class Login(View):
+    Login_Form = AuthenticationForm
 
-        if form_login.is_valid():
+    def get(self, request):
+        form = self.Login_Form()
+        return render(request, template_name='blog/login.html',
+                      context={'form_login': form})
+
+    def post(self, request):
+
+        form = self.Login_Form(data=request.POST)
+
+        if form.is_valid():
             user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
             login(request, user)
             # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-            return redirect(reverse('post_list'))
+            return redirect(reverse('blog:post_list'))
             # return render(request, 'blog/post_list.html', context={'posts': posts, 'user': request.user})
 
-    else:
-        form_login = AuthenticationForm()
-    return render(request, template_name='blog/login.html',
-                  context={'form_login': form_login})
+        else:
+            form = self.Login_Form()
+            return render(request, template_name='blog/login.html',
+                          context={'form_login': form})
 
 
-def post_new(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
+class NewPost(View):
+    form = PostForm
+
+    def get(self, request):
+
+        new_form = self.form()
+        return render(request, 'blog/post_edit.html', {'form': new_form})
+
+    def post(self, request):
+        new_form = self.form(request.POST)
+        if new_form.is_valid():
+            post = new_form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_list')
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+            return redirect('blog:post_list')
+        else:
+            new_form = self.form()
+        return render(request, 'blog/post_edit.html', {'form': new_form})
+
+
+# def post_list(request):
+#     login_user = UserBlog.objects.get(username=request.user.username)
+#
+#     posts = Post.objects.filter(Q(author__in=login_user.followers.all()) | Q(author=request.user))
+#     return render(request, 'blog/post_list.html', context={'posts': posts, 'user': request.user})
+
+
+# def signup_view(request):
+#     if request.method == 'POST':
+#
+#         form_sign = UserCreationForm(request.POST)
+#
+#         if form_sign.is_valid():
+#             user = form_sign.save()
+#             logout(request)
+#             login(request, user)
+#             # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+#
+#             return redirect(reverse('post_list'))
+#             # return render(request, 'blog/post_list.html', context={'posts': posts, 'user': request.user})
+#     else:
+#         form_sign = UserCreationForm()
+#     return render(request, template_name='blog/signup.html',
+#                   context={'form_sign': form_sign})
+#
+
+# def login_view(request):
+#     if request.method == 'POST':
+#         form_login = AuthenticationForm(data=request.POST)
+#
+#         if form_login.is_valid():
+#             user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+#             login(request, user)
+#             # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+#             return redirect(reverse('blog:post_list'))
+#             # return render(request, 'blog/post_list.html', context={'posts': posts, 'user': request.user})
+#
+#     else:
+#         form_login = AuthenticationForm()
+#     return render(request, template_name='blog/login.html',
+#                   context={'form_login': form_login})
+#
+
+# def post_new(request):
+#     if request.method == 'POST':
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.published_date = timezone.now()
+#             post.save()
+#             return redirect('post_list')
+#     else:
+#         form = PostForm()
+#     return render(request, 'blog/post_edit.html', {'form': form})
 
 
 def logout_view(request):
     logout(request)
 
-    return redirect('login')
+    return redirect(reverse('blog:login'))
 
 
 def post_detail(request, pk):
