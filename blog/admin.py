fd5a657851e39816fd6f603306a96db201c8c60a
herman72 <m.sherafati7@gmail.com>
2020-09-26 10:45:37 +0330

    castomize admin page

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 
 from .models import Post, Comment, UserBlog
 
-admin.site.register(Post)
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+    readonly_fields = ['author_comment', 'text', 'created_time', 'post']
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_time')
+    fields = ['title', 'author', 'created_time', 'text']
+    # def
+    inlines = [CommentInline]
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
 admin.site.register(UserBlog)
-
